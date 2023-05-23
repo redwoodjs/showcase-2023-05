2f251eb35422036d98cc6bd3e361ede00a5b4205
David Price <thedavid@thedavidprice.com>
2023-05-23 16:48:00 -0700

    move project into latest RW codebase

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,4 +1,3 @@
-import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
 
 export default async () => {
@@ -9,7 +8,7 @@ export default async () => {
     //
     // Update "const data = []" to match your data model and seeding needs
     //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
+    const data = [
       // To try this example data with the UserExample model in schema.prisma,
       // uncomment the lines below and run 'yarn rw prisma migrate dev'
       //
@@ -28,11 +27,34 @@ export default async () => {
       //
       // Change to match your data model and seeding needs
       //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
+      data.map(async (data) => {
         const record = await db.userExample.create({ data })
         console.log(record)
       })
     )
+
+    // If using dbAuth and seeding users, you'll need to add a `hashedPassword`
+    // and associated `salt` to their record. Here's how to create them using
+    // the same algorithm that dbAuth uses internally:
+    //
+    //   import { hashPassword } from '@redwoodjs/auth-dbauth-api'
+    //
+    //   const users = [
+    //     { name: 'john', email: 'john@example.com', password: 'secret1' },
+    //     { name: 'jane', email: 'jane@example.com', password: 'secret2' }
+    //   ]
+    //
+    //   for (user of users) {
+    //     const [hashedPassword, salt] = hashPassword(user.password)
+    //     await db.user.create({
+    //       data: {
+    //         name: user.name,
+    //         email: user.email,
+    //         hashedPassword,
+    //         salt
+    //       }
+    //     })
+    //   }
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
